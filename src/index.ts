cd8f65bd648ff70f52459490779783031a553ef6
pierre <oxeyo@hotmail.fr>
2019-05-19 23:05:49 +0200

    exposed header

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as express from "express";
 //options for cors midddleware
 const options:cors.CorsOptions = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","Access-Control-Allow-Origin"],
+  exposedHeaders :["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","Access-Control-Allow-Origin"],
   credentials: true,
   methods: ["GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE"],
   origin: true,
