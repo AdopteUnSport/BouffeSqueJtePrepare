8f1e8e08b68c2bc06297f61f7ce570a20d9218d6
pierre <oxeyo@hotmail.fr>
2019-05-19 19:55:27 +0200

    core policy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import app from "./App";
-const PORT = 3000;
+import * as cors from "cors";
 
+//get router
+
+//options for cors midddleware
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
+  preflightContinue: false
+};
+
+//use cors middleware
+app.use(cors(options));
+
+//add your routes
+
+//enable pre-flight
+app.options("*", cors(options));
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
