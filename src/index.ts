03601b8f85a4655821763edfe106729d90fa625c
pierre <oxeyo@hotmail.fr>
2019-05-19 22:35:16 +0200

    app

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,22 @@ import app from "./App";
 import * as cors from "cors";
 import * as express from "express";
 //get router
-const router = express.Router()
 //options for cors midddleware
 const options:cors.CorsOptions = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","Access-Control-Allow-Origin"],
   credentials: true,
-  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: "*",
+  methods: ["GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE"],
+  origin: ["*"],
   preflightContinue: false
 };
 
 //use cors middleware
-router.use(cors(options));
+app.use(cors(options));
 
 //add your routes
 
 //enable pre-flight
-router.options("*", cors(options));
+app.options("*", cors(options));
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
