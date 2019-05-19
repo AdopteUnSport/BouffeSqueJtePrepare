b871beac7bdc4903a382629a41d79f7ac89d8beb
pierre <oxeyo@hotmail.fr>
2019-05-19 23:02:46 +0200

    d

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const options:cors.CorsOptions = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","Access-Control-Allow-Origin"],
   credentials: true,
   methods: ["GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE"],
-  origin: "http://localhost:4200",
+  origin: true,
   preflightContinue: false
 };
 
