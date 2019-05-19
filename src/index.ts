700a2b0df06deec43bc511ccac9031b12561c3c7
pierre <oxeyo@hotmail.fr>
2019-05-19 20:53:55 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as cors from "cors";
 
 //options for cors midddleware
 const options:cors.CorsOptions = {
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token","Access-Control-Allow-Origin"],
   credentials: true,
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
   origin: "*",
