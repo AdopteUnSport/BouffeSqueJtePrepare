1e0c14dc233841e776d8481e2107092c715ff400
pierre <oxeyo@hotmail.fr>
2019-06-10 22:23:05 +0200

    API doc on /api-docs/swagger

--- src/App.ts
+++ src/App.ts
@@ -30,6 +30,8 @@ class App {
   }
 
   private config(): void{
+      this.app.use( '/api-docs/swagger', express.static( 'swagger' ) );
+      this.app.use( '/api-docs/swagger/assets', express.static( 'node_modules/swagger-ui-dist' ) );
       this.app.use(bodyParser.json());
       this.app.use(bodyParser.urlencoded({ extended: false }));
   }
